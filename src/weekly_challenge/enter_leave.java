7be008895a64f74e5950f5776909a18b0c36281e
jisoo <ske050588@gmail.com>
2021-09-24 14:33:33 +0900

    weekly challenge

--- src/weekly_challenge/enter_leave.java
+++ src/weekly_challenge/enter_leave.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 public class enter_leave {
 
 	public static void main(String[] args) {
-		int[] enter = {1, 10, 9, 2, 3, 8, 7, 4, 5, 6};
-		int[] leave = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		int[] enter = {1, 3, 2, 4, 6, 5, 8, 7, 9, 10};
+			//{1, 10, 9, 2, 3, 8, 7, 4, 5, 6};
+		int[] leave = {9, 5, 1, 10, 7, 4, 8, 6, 2, 3};
+			//{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		//기댓값 8, 9, 9, 9, 8, 9, 9, 9, 8, 6
 		//기댓값 9, 7, 7, 5, 5, 5, 3, 3, 1, 1
-		//이 테스트 케이스만 안됨
+
 		int[] meet = new int[enter.length];
 		int leaveTH = 0;
 		HashSet<Integer> set = new HashSet<>();
@@ -16,7 +19,7 @@ public class enter_leave {
 			set.add(enter[i]);
 
 			if(set.contains( leave[leaveTH])) {
-				for(int inner : set) meet[ inner-1 ] += (set.size()-1);
+				for(int inner : set) meet[ inner-1 ] = Math.max( meet[inner-1], (set.size()-1));
 			}
 
 			while(set.contains( leave[leaveTH] )) {
