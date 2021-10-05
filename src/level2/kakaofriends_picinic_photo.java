4a89359fdb5b23e3812505bfe05be7b2d3907670
jisoo <ske050588@gmail.com>
2021-10-05 23:37:53 +0900

    단체사진 찍기 dfs

---
+++ src/level2/kakaofriends_picinic_photo.java
@@ -0,0 +1,23 @@
+package level2;
+//https://programmers.co.kr/learn/courses/30/lessons/1835
+//단체사진 찍기
+public class kakaofriends_picinic_photo {
+
+	public static void main(String[] args) {
+		int n=2;
+		String[] data = {"N~F=0", "R~T>2"};
+		char[] names = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+		int answer = 0;
+		//단체사진 찍는 카카오프렌즈는 총 8명임
+
+
+
+	}
+
+	static public int dfs() {
+
+
+		return 0;
+	}
+
+}
