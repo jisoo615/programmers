fa812a6af55c5892d99f241cc1e33dc97d8229a9
jisoo <ske050588@gmail.com>
2021-10-09 17:57:15 +0900

    우선순위 큐 사용한 문제

--- src/level2/moreSpicy.java
+++ src/level2/moreSpicy.java
@@ -1,17 +1,17 @@
 package level2;
-
+//https://programmers.co.kr/learn/courses/30/lessons/42626
 import java.util.PriorityQueue;
 
 public class moreSpicy {
 
 	public static void main(String[] args) {
-		int[] scoville = {7, 7, 7, 7};
-		int k = 7;
+		int[] scoville = {1, 2, 3, 4, 5};
+		int K = 5;//K는 대문자
 		int answer = 0;
 		PriorityQueue<Integer> q = new PriorityQueue<>();
-		for(int x:scoville) q.add(x);
+		for(int x:scoville) q.offer(x);
 
-		while(q.peek() < k) {
+		while(q.peek() < K) {
 			if(q.size()==1) {
 				answer = -1;
 				break;
