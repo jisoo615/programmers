113d2825f37c906d82fff911242990a217de1eb9
jisoo <ske050588@gmail.com>
2021-11-09 17:09:02 +0900

    카카오 인턴 2021 표편집 완료. 생각잘하기

--- src/kako/kakao_2021_intern_editTable.java
+++ src/kako/kakao_2021_intern_editTable.java
@@ -6,27 +6,27 @@ public class kakao_2021_intern_editTable {
 
 	static public void main(String[] args) {
 		int n=8; int k=2;//행개수, 선택된 행
-		String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};//명령어
+		String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z", "U 1", "C"};//명령어
 		System.out.print(solution(n, k, cmd));
 	}
 
 	static public String solution(int n, int k, String[] cmd) {
 		LinkedList<Integer> list = new LinkedList<>();
 		for(String str : cmd) {
-			int move = str.length()<=1 ? 0:Integer.valueOf(str.charAt(2));
+			int move = str.length()<2 ? 0:Integer.valueOf(str.substring(2));
 			int index = 0;
 
 			switch(str.charAt(0)) {
-			case 'U' : k = Math.max(0, k-move);//현재보다 위
+			case 'U' : k = Math.max(0, k-move);//현재보다 위/최대한이 맨 위에끝
 				break;
-			case 'D' : k = Math.min(k-move, n-1);//현재보다 아래
+			case 'D' : k = Math.min(k+move, n-1);//현재보다 아래/최대한이 맨 아래끝
 				break;
-			case 'C' : list.add(k);
+			case 'C' : list.push(k);
 						n-=1;
-						if(k==n) k-=1;
+						if(k==n) k-=1;//전 위치가 맨 마지막이었으면 하나 위로
 				break;
-			case 'Z' : index = list.pollLast();
-						if(index<=k) k+=1;
+			case 'Z' : index = list.pop();
+						if(index<=k) k+=1;//추가된게 더 위에였으면 현재위치+1
 						n += 1;
 				break;
 			}
@@ -35,7 +35,7 @@ public class kakao_2021_intern_editTable {
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<n; i++) sb.append('O');
 		while(list.size()>0) {
-			int idx = list.pollLast();
+			int idx = list.pop();
 			sb.insert(idx, 'X');
 		}
 
