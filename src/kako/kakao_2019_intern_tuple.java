71f845b506d3ce2abb44f4e9fbfe9958ec600252
jisoo <ske050588@gmail.com>
2021-11-10 17:33:28 +0900

    카카오 인턴 2019 튜플 완료

--- src/kako/kakao_2019_intern_tuple.java
+++ src/kako/kakao_2019_intern_tuple.java
@@ -1,31 +1,48 @@
 package kako;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class kakao_2019_intern_tuple {
 //스킬체크 레벨2문제와 같음 풀었던거 수정해서 풀기
 		public static void main(String[] args) {
-			solution_2("{{2,1},{2},{2,1,3,4},{2,1,3}}");
+			String s = "{{2,1},{2},{2,1,3,4},{2,1,3}}";
+			for(int x :solution_2(s)) {
+				System.out.print(x+" ");
+			}
 
 		}
 
 		static public int[] solution_2(String s) {
 			//중괄호 없애주고 hashMap에 ,단위로 개수를 추가해줌
-			//가장 많은 개슈의 숫자가 맨 앞임
-			s = s.replace("[{}]", "");
-			String[] arr = s.split(",");
+			//가장 많은 개수의 숫자가 원래집합에서 맨 앞임
+			String s2 = s.replaceAll("[{}]", "");
+
+			String[] arr = s2.split(",");
 			HashMap<String, Integer> map = new HashMap<>();
 			for(String num : arr) {
-				if(map.containsKey(num)) {
+				if(!map.containsKey(num)) {
 					map.put(num, 1);
 				}else {
 					map.put(num, map.get(num)+1);
 				}
 			}
 
-			String[] arr2 = map.entrySet().toArray();
+			List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(map.entrySet());
+			Collections.sort(list, (o1, o2)->{
+				return o2.getValue() - o1.getValue();
+			});
 
-			return new int[] {};
+			int[] answer = new int[list.size()];
+			int index = 0;
+			for(Entry<String, Integer> entry : list) {
+				answer[index++] = Integer.parseInt(entry.getKey());
+			}
+
+			return answer;
 		}
 
 	}
