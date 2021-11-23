50c48df1f83622ef2fc36de4c6b7ff80de76a6c2
jisoo <ske050588@gmail.com>
2021-11-23 17:08:56 +0900

    카카오 2021 순위검색 효율성까지 완료. 검색시 ArrayList가 더 빠름 주의

--- src/kako/kakao_2021_blind_rankSearch.java
+++ src/kako/kakao_2021_blind_rankSearch.java
@@ -1,14 +1,15 @@
 package kako;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 //https://programmers.co.kr/learn/courses/30/lessons/72412
 //카카오 블라인드 2021 순위검색
+//효울성 통과 : LinkedList를 ArrayList로 바꾸니 성공.
+//왜냐 어레이리스트가 검색이 더 빠름-삽입삭제도 좀 있긴 하지만 binarySearch할때 검색기 더 많음
 public class kakao_2021_blind_rankSearch {
-	static public HashMap<String, LinkedList<Integer>> map = new HashMap<>();
+	static public HashMap<String, ArrayList<Integer>> map = new HashMap<>();
 
 	public static void main(String[] args) {
 		String[] info = {"java backend junior pizza 150","python frontend senior chicken 210",
@@ -31,21 +32,20 @@ public class kakao_2021_blind_rankSearch {
         int[] answer = new int[query.length];
         //해시테이블에 ㅑ info에서 -를 넣은 모든 경우의 수를 key값으로 저장하고, value를 점수로 저장.
         //value 는 integer형의 리스트여야 한다. 같은 키값이 존재할 수 있기에
-        boolean[] visited = new boolean[4];
         for(String str : info) {
-        	Arrays.fill(visited, false);
         	StringBuilder sb = new StringBuilder();
     	    combi(str.split(" "), sb, 0);
        }
+
+        for(String key: map.keySet()) {//이진탐색 전에 미리 정렬하기
+        	Collections.sort(map.get(key));
+        }
 
         for(int i=0; i<query.length; i++) {
         	int score = Integer.parseInt(query[i].replaceAll("[a-z-]+", "").strip());
         	String condition = query[i].replace("and", "").replaceAll("[0-9\\s]+", "");
     		// 정규식에서 공백은 \\s 로 표현됨
-        	if(map.containsKey(condition)) {//찾는 조건이 있다면
-        		LinkedList<Integer> scores = map.get(condition);
-        		answer[i] = binarySearch(score, scores);
-        	}
+    		answer[i] = binarySearch(condition, score);
         }
 
         return answer;
@@ -54,12 +54,11 @@ public class kakao_2021_blind_rankSearch {
 	static public void combi(String[] arr, StringBuilder sb, int index) {//dfs 조건 4개 점수 1개
 		if(index==4) {//모든 항목 확인했으면
 			if(map.containsKey(sb.toString())) {
-				LinkedList<Integer> list = map.get(sb.toString());
+				ArrayList<Integer> list = map.get(sb.toString());
 				list.add(Integer.parseInt(arr[4]));
-				Collections.sort(list);
 				map.put(sb.toString(), list);
 			}else {
-				LinkedList<Integer> list = new LinkedList<Integer>();
+				ArrayList<Integer> list = new ArrayList<Integer>();
 				list.add(Integer.parseInt(arr[4]));
 				map.put(sb.toString(), list);
 			}
@@ -74,7 +73,11 @@ public class kakao_2021_blind_rankSearch {
 		}
 	}
 
-	public static int binarySearch(int score, LinkedList<Integer> list) {//score 이상인 개수를 반환
+	public static int binarySearch(String condition, int score) {//score 이상인 개수를 반환
+		if(!map.containsKey(condition)) return 0;//없다면 0
+
+		ArrayList<Integer> list = map.get(condition);
+
 		int left = 0; int right = list.size();
 		while(left<right) {
 			int mid = (left+right)/2;
