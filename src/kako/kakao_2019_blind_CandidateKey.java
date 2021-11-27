13106ca1a4faed3e4eaf70e3724108dd749bf910
jisoo <ske050588@gmail.com>
2021-11-27 23:36:14 +0900

    카카오 2019 블라인드 후보키 유일성 최소성 순으로 구현하기 50%

--- src/kako/kakao_2019_blind_CandidateKey.java
+++ src/kako/kakao_2019_blind_CandidateKey.java
@@ -1,65 +1,62 @@
 package kako;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 
 public class kakao_2019_blind_CandidateKey {
 
 	public static void main(String[] args) {
-		String[][] relation= {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+		//String[][] relation= {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+		String[][] relation = { {"a","1","aaa","c","ng"},
+								{"a","1","bbb","e","g"},
+								{"c","1","aaa","d","ng"},
+								{"d","2","bbb","d","ng"}};
 		System.out.print(solution(relation));
 
 	}
-	public static LinkedList<String> list;
+	public static ArrayList<Integer> list;
 	static public int solution(String[][] relation) {
 		//최소성 : 두개이상의 속성이 후보키가 될때, 속성을 하나 제거하고서도 유일성을 만족하면 후보키가 될수없다.
-        //1. 속성들로 만들 수 있는 모든 후보키의 경우의수를 구한다.
-		//2. 모든 경우의 수 중 유일성을 만족하는 경우만 list에 저장한다.
-		list = new LinkedList<>();
-        cases(relation[0].length, relation);
+        //1. 속성들로 만들 수 있는 모든 후보키의 경우의수 들 중(1 ~ 2^속성)
+		//2. 이후 최소성을 만족하도록 비트연산&을 통해 list저장하고 서로 비교, 2진수인 String으로 저장해두기
+		int colsize = relation[0].length;
+		list = new ArrayList<>();
+		int whole = (int)Math.pow(2, colsize);
+		for(int n=1; n<whole; n++) {
+			String str = binary(n, colsize);
+			if(isUnique(str, relation)) {
+				list.add(n);//유일성 만족하는 것만 add함
+			}
+		}
 
-        //3. 이후 최소성을 만족하도록 최소개수의후보키를 포함한 모든 키들을 제거함.
-        Collections.sort(list, (str1, str2)->{
-        	int cnt1=0, cnt2 =0;
-        	for(int ii=0; ii<str1.length(); ii++) {
-        		if(str1.charAt(ii)=='1') cnt1++;
-        		if(str2.charAt(ii)=='1') cnt2++;
-        	}
-        	return cnt1-cnt2;
-        });
+       //3. 최소성만족(부분집합이 없는) 조합들을 set에 레코드들을 넣어서 중복인 조합을 제거한다.
 
-        /*
-         * 최소성 만족시키기
-         */
+		for(int i=0; i<list.size(); i++){//지울 대상들
+			for(int j=0; j<i; j++) {
+				if((list.get(i) & list.get(j))==list.get(i)) {
+					list.remove(j);
+					j--;
+				}
+			}
+		}
 
-        return 0;
+        return list.size();
     }
 
-	static public void cases(int attribute, String[][] relation) {//모든 경우의 수를 2진수로 나타내자 (속성갯수길이만큼)
-		int num=1, n=0;
-		int max = (int) Math.pow(2, attribute);
+	static public String binary(int n, int colsize) {//받은 수를 2진수로 나타내자 (속성갯수길이만큼)
+
 		StringBuilder sb = new StringBuilder();
-		while(num<max) {
-			sb = new StringBuilder();
-			n = num;
-			while(n>1) {
-				sb.append(Integer.toString(n%2));
-				n/=2;
-			}
-			if(n==1)sb.append("1");
-			else sb.append("0");
-
-			String str = "0".repeat(attribute-sb.length());
-			sb.append(str);
-			sb.reverse();
-			if(isUnique(sb.toString(), relation)){
-				list.add(sb.toString());
-				System.out.println(sb.toString());
-			}
-
-			num++;
+		sb = new StringBuilder();
+		while(n>1) {
+			sb.append(Integer.toString(n%2));
+			n/=2;
 		}
+		if(n==1)sb.append("1");
+		else sb.append("0");
+
+		String str = "0".repeat(colsize-sb.length());
+		sb.append(str);
+		return sb.reverse().toString();
 	}
 
 	static public boolean isUnique(String str, String[][] relation) {
