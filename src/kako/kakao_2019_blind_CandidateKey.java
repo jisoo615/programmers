2e57a718e194f4eba03f98799424d56318f97865
jisoo <ske050588@gmail.com>
2021-11-29 11:09:02 +0900

    kakao 2019 후보키 완료. 비트연산(부분집합 없애기)

--- src/kako/kakao_2019_blind_CandidateKey.java
+++ src/kako/kakao_2019_blind_CandidateKey.java
@@ -18,7 +18,7 @@ public class kakao_2019_blind_CandidateKey {
 	static public int solution(String[][] relation) {
 		//최소성 : 두개이상의 속성이 후보키가 될때, 속성을 하나 제거하고서도 유일성을 만족하면 후보키가 될수없다.
         //1. 속성들로 만들 수 있는 모든 후보키의 경우의수 들 중(1 ~ 2^속성)
-		//2. 이후 최소성을 만족하도록 비트연산&을 통해 list저장하고 서로 비교, 2진수인 String으로 저장해두기
+		//2. 유일성을 먼저 검사하기 (2진수인 String으로 변환해 isUique에 넘겨주기)
 		int colsize = relation[0].length;
 		list = new ArrayList<>();
 		int whole = (int)Math.pow(2, colsize);
@@ -29,10 +29,10 @@ public class kakao_2019_blind_CandidateKey {
 			}
 		}
 
-       //3. 최소성만족(부분집합이 없는) 조합들을 set에 레코드들을 넣어서 중복인 조합을 제거한다.
+       //3. 최소성만족(부분집합이 없는) 하도록 비트여산을 이요해 알아낸다.
 
 		for(int i=0; i<list.size(); i++){//지울 대상들
-			for(int j=0; j<i; j++) {
+			for(int j=i+1; j<list.size(); j++) {
 				if((list.get(i) & list.get(j))==list.get(i)) {
 					list.remove(j);
 					j--;
