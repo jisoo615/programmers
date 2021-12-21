b64f66c6e2be2d30be0e51311718495380dff256
jisoo <ske050588@gmail.com>
2021-12-21 11:46:49 +0900

    레벨2 게임 맵 최단거리. 과거 효율성 테스트 실패한것 고침. Queue<int[]>

--- src/level2/gameMapShortestDistance.java
+++ src/level2/gameMapShortestDistance.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 
 //https://programmers.co.kr/learn/courses/30/lessons/1844
 //게임 맵 푀단거리
+//이번엔 통과된 이유 : 배열을 인큐디큐했다. x값과 y값을 따로 인큐디큐하면 시간이 두배로 걸리기 때문
 public class gameMapShortestDistance {
 
 	public static void main(String[] args) {
@@ -13,19 +14,19 @@ public class gameMapShortestDistance {
 	}
 
 	static public int solution(int[][] maps) {
-		Queue<Integer> que = new LinkedList<>();
+		Queue<int[]> que = new LinkedList<>();
         int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         int x_max = maps.length-1;
         int y_max = maps[0].length-1;
         int nowX, nowY, nextX, nextY;
         boolean visited[][] = new boolean[x_max+1][y_max+1];
-        que.offer(0); que.offer(0);
+        que.offer(new int[] {0, 0});
         visited[0][0] = true;
 
         while(!que.isEmpty()){
-            nowX = que.poll();
-            nowY = que.poll();
+        	int[] now = que.poll();
+        	nowX = now[0]; nowY = now[1];
             System.out.print("("+nowX+", "+nowY+")"+" ");
 
             for(int i=0; i<4; i++){
@@ -35,7 +36,7 @@ public class gameMapShortestDistance {
                 if(nextX <0 || nextX>x_max || nextY<0 || nextY>y_max) continue;
                 if(maps[nextX][nextY]==0) continue;
                 if(!visited[nextX][nextY]){
-                    que.offer(nextX); que.offer(nextY);
+                    que.offer(new int[] {nextX, nextY});
                     maps[nextX][nextY] = maps[nowX][nowY] + 1;
                     visited[nextX][nextY] = true;
                     }
