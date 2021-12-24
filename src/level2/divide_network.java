501dcd1b08c2132baa5c390a25abf559103eb779
jisoo <ske050588@gmail.com>
2021-12-24 17:20:57 +0900

    레벨2 전력망을 둘로 나누기. 완료. 문제도출이 살짝 어렵. 간선을 하나씩 끊어보기.

--- src/level2/divide_network.java
+++ src/level2/divide_network.java
@@ -1,4 +1,8 @@
 package level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 //https://programmers.co.kr/learn/courses/30/lessons/86971
 //전력망을 둘로 나누기
 public class divide_network {
@@ -13,15 +17,51 @@ public class divide_network {
 	}
 
 	static class Solution {
-	    public int solution(int n, int[][] wires) {
-	        int answer = -1;
-	        boolean visited[] = new boolean[101];
-	        for(int[] node : wires){
+		//간선을 하나씩 끊어보며 차이가 가장 적게 날때의 차를 반환한다.
+		public static int[][] graph;
 
+	    public int solution(int n, int[][] wires) {
+	        int min =n;
+	        graph = new int[n+1][n+1];
+	        for(int[] wire : wires) {
+	        	graph[wire[0]][wire[1]] = graph[wire[1]][wire[0]] = 1;
 	        }
 
-	        return answer;
+	        for(int[] wire : wires) {
+	        	//맨 처음부터 마지막까지 하나의 간선만 제외하기
+	        	graph[wire[0]][wire[1]] = graph[wire[1]][wire[0]] = 0;
+	        	int differ = Math.abs(dfs(wire[0]) - dfs(wire[1]));
+	        	min = Math.min(min, differ);
+	        	graph[wire[0]][wire[1]] = graph[wire[1]][wire[0]] = 1;//원상태로 돌려놓기
+	        }
+
+	        return min;
 	    }
+
+	    public int dfs(int start) {
+	    	//두로 나눠진 각 그래프의 노드를 구하고 차를 반환하라
+	    	Queue<Integer> que = new LinkedList<>();
+	    	boolean[] visited = new boolean[graph.length];
+
+	    	que.add(start);
+	    	visited[start] = true;
+	    	while(!que.isEmpty()) {
+	    		int node = que.poll();
+	    		for(int i=1; i<graph.length; i++) {
+	    			if(graph[node][i]>0 && visited[i]==false) {
+	    				visited[i] = true;
+	    				que.add(i);
+	    			}
+	    		}
+	    	}
+
+	    	int cnt=0;
+	    	for(int i=1; i<visited.length; i++) {
+	    		if(visited[i]) cnt++;
+	    	}
+
+	    	return cnt;
+	}
 	}
 
 }
