3f8c2a24811fc6371b2a54ec47f778cccfce664d
jisoo <ske050588@gmail.com>
2022-01-14 12:57:31 +0900

    카카오 2022 사라지는 발판 미완. 클래스 생성만 함.

---
+++ src/kako/kakao_2022_disappear_footstand.java
@@ -0,0 +1,22 @@
+package kako;
+//https://programmers.co.kr/learn/courses/30/lessons/92345
+//카카오 2022 사라지는 발판
+public class kakao_2022_disappear_footstand {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[][] board = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+		int[] aloc = {1, 0};
+		int[] bloc = {1, 2};
+		System.out.print(s.solution(board, aloc, bloc));
+	}
+
+	static class Solution {
+	    public int solution(int[][] board, int[] aloc, int[] bloc) {
+	        int answer = -1;
+
+	        return answer;
+	    }
+	}
+
+}
